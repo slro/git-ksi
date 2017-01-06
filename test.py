9628b6491cf63772c008c4496ca584b646465976
vsmysle <kyrylo.voronchenko@gmail.com>
2017-01-06 18:10:56 +0000

    added test POST requests

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
-import urllib.request
+import requests
+import json
+
+url = "https://tryout-catena-ksi.guardtime.net/api/v1/signature"
+headers = {"Content-Type": "application/json",
+           "Accept": "application/json",
+           "Authorization":'Basic "ot.S1xpgT:xRKHrjcSFTw6"'}
+
+with open("dataHash.json") as json_file:
+    data = json.load(json_file)
+
+r = requests.post(url,headers=headers,json=data)
+print(r.text)
